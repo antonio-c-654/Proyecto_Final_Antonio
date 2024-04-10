0793c7af5bb5581cb36ee046a357b456f7b8d043
Antonio Callau <antonioluiscallaucano@gmail.com>
2024-04-10 18:29:59 +0200

    añadido vue toastification

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css"; // Import the CSS or use your own!
+
+const options = {
+    // You can set your default options here
+};
+
+createApp(App).use(store).use(router).use(Toast, options).mount('#app')
--- src/views/PagoView.vue
+++ src/views/PagoView.vue
@@ -6,21 +6,25 @@
 
     <div class="flex flex-col gap-4">
       <p class="">Seleccione metodo de pago</p>
-      <div>
+      <div class="md:w-[50vw] border border-red-600">
         <h1 class="text-[#14c458]">Tarjeta</h1>
-        <img src="https://static.vecteezy.com/system/resources/thumbnails/009/384/393/small_2x/credit-card-clipart-design-illustration-free-png.png" class="w-[50vw]">
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/009/384/393/small_2x/credit-card-clipart-design-illustration-free-png.png" class="w-[50vw] md:w-[20vw]">
         <p>Numero de tarjeta</p>
         <input type="text">
-        <p>Codigo de tarjeta</p>
+        <p>Fecha de caducidad</p>
+        <input type="text">
+        <p>Codigo CCV de tarjeta</p>
         <input type="text">
       </div>
       <h1 class="text-[#14c458]">Otros metodos de pago online</h1>
-      <div v-for="met in metodos_pago" :key="met.id" class="border flex items-center justify-between h-[10vh] py-2 px-2">
+      <div v-for="met in metodos_pago" :key="met.id" class="border flex items-center justify-between h-[10vh] md:w-[50vw] py-2 px-2">
         <p class="w-[30%]">{{ met.nombre }}</p>
-        <img :src="met.logo" class="w-[40%]">
+        <img :src="met.logo" class="w-[40%] md:w-[5vw]">
         <input type="radio" id="met_pago" name="met_pago" :value="met.id">
       </div>
     </div>
+
+    <button @click="alertaToast">Toast</button>
 
   </div>
 
@@ -28,11 +32,25 @@
 
 <script>
 import { mapState } from 'vuex';
+import { useToast } from "vue-toastification";
 
 export default {
   computed:{
     ...mapState(['metodos_pago'])
-  }
+  },
+  setup() {
+      const toast = useToast();      
+      return { toast }
+    },
+    methods: {
+      alertaToast() {
+        // this.toast.success("Correcto");
+        this.toast.success("Correcto", {
+        timeout: 2000
+      });
+      }
+    }
+
 }
 </script>
 
