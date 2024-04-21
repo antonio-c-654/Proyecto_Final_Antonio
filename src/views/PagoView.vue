7032461e982ce7ec19c56ddf8683f83415732369
Antonio Callau <antonioluiscallaucano@gmail.com>
2024-04-21 21:38:04 +0200

    mejorado PagoView

--- src/views/PagoView.vue
+++ src/views/PagoView.vue
@@ -1,40 +1,62 @@
 <template>
 
-  <div class="bg-[#111015] h-auto min-h-[100vh] md:h-[100vh] text-white flex flex-col p-2">
+  <div class="bg-[#111015] h-auto min-h-[100vh] md:h-[100vh] text-white flex flex-col md:flex-row">
 
-    <h1 class="text-[#14c458] text-3xl">Metodos de pago</h1>
+    <div id="parte_izq" class="md:w-[50%] h-full p-4">
+    
+      <h1 class="text-[#14c458] text-3xl">Métodos de pago</h1>
+      <div class="flex flex-col gap-4">
+        <p class="">Seleccione metodo de pago</p>
+        <h1 class="text-[#14c458]">Tarjeta</h1>
 
-    <div class="flex flex-col gap-4">
-      <p class="">Seleccione metodo de pago</p>
-      <h1 class="text-[#14c458]">Tarjeta</h1>
+        <div class="md:w-[50vw] md:flex">
 
-      <div class="md:w-[50vw] border border-red-600 md:flex">
+          <!-- <div class="card_atropos">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/009/384/393/small_2x/credit-card-clipart-design-illustration-free-png.png" class="w-[70vw] md:w-[20vw] mb-2">
+          </div> -->
 
-        <!-- <div class="card_atropos">
-          <img src="https://static.vecteezy.com/system/resources/thumbnails/009/384/393/small_2x/credit-card-clipart-design-illustration-free-png.png" class="w-[70vw] md:w-[20vw] mb-2">
-        </div> -->
+          <CardAtroposVue></CardAtroposVue>
 
-        <CardAtroposVue></CardAtroposVue>
+          <div class="md:flex md:flex-col md:justify-center md:w-[50%] md:pl-8">
+            <p>Numero de tarjeta</p>
+            <input type="text" class="p-1 rounded-md w-[90%] md:w-[90%] bg-[#111015] border border-[#14c458] mb-2">
+            <p>Fecha de caducidad</p>
+            <input type="text" class="p-1 rounded-md w-[90%] md:w-[90%] bg-[#111015] border border-[#14c458] mb-2">
+            <p>Codigo CCV de tarjeta</p>
+            <input type="text" class="p-1 rounded-md w-[90%] md:w-[90%] bg-[#111015] border border-[#14c458] mb-2">
+          </div>
+        </div>
 
-        <div class="md:flex md:flex-col md:justify-center md:w-[50%] md:pl-8">
-          <p>Numero de tarjeta</p>
-          <input type="text" class="p-1 rounded-md w-[90%] md:w-[90%] bg-[#111015] border border-[#14c458] mb-2">
-          <p>Fecha de caducidad</p>
-          <input type="text" class="p-1 rounded-md w-[90%] md:w-[90%] bg-[#111015] border border-[#14c458] mb-2">
-          <p>Codigo CCV de tarjeta</p>
-          <input type="text" class="p-1 rounded-md w-[90%] md:w-[90%] bg-[#111015] border border-[#14c458] mb-2">
+        <h1 class="text-[#14c458]">Otros metodos de pago online</h1>
+        <div v-for="met in metodos_pago" :key="met.id" class="border flex items-center justify-between h-[10vh] md:w-[30vw] py-4 px-6 rounded-lg bg-[#22222a] hover:border-[#14c458]">
+          <p class="w-[30%]">{{ met.nombre }}</p>
+          <img :src="met.logo" class="w-[40%] md:w-[5vw]">
+          <div class="w-[30%] h-full flex justify-end">
+            <input type="radio" id="met_pago" name="met_pago" :value="met.id">
+          </div>
         </div>
       </div>
 
-      <h1 class="text-[#14c458]">Otros metodos de pago online</h1>
-      <div v-for="met in metodos_pago" :key="met.id" class="border flex items-center justify-between h-[10vh] md:w-[50vw] py-2 px-2 bg-[#22222a]">
-        <p class="w-[30%]">{{ met.nombre }}</p>
-        <img :src="met.logo" class="w-[40%] md:w-[5vw]">
-        <input type="radio" id="met_pago" name="met_pago" :value="met.id">
-      </div>
+      <button @click="alertaToast" class="text-white w-[120px] h-[35px] rounded-md cursor-pointer hover:border hover:border-white-600 shadow-md bg-gradient-to-br from-blue-500 to-teal-400 my-8">Pagar</button>
+
     </div>
 
-    <button @click="alertaToast" class="text-white w-[120px] h-[35px] rounded-md cursor-pointer hover:border hover:border-white-600 shadow-md bg-gradient-to-br from-blue-500 to-teal-400 my-4">Pagar</button>
+    <div id="parte_der" class="md:w-[50%] h-full p-4">
+      <h1 class="text-[#14c458] text-3xl mb-4">Resumen de la compra</h1>
+      <div class="flex flex-col gap-2">
+        <div id="cabecera" class="bg-teal-400 w-full flex items-center justify-between py-4 px-6">
+          <span class="text-sm">Nombre</span>
+          <span class="text-sm">Cocción</span>
+          <span class="text-sm">Precio</span>
+        </div>
+        <div v-for="i in 4" :key="i" class="w-full flex items-center justify-between bg-[#22222a] rounded-md p-6">
+          <span class="text-sm">Hamburguesa Deluxe</span>
+          <span class="text-sm">Al punto</span>
+          <span class="text-sm text-teal-400">14.99 €</span>
+        </div>
+      </div>
+      <h1 class="text-2xl my-6">Total: <span class="text-[#14c458]">113€</span> </h1>
+    </div>
 
   </div>
 
