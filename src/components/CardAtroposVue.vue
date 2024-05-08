af87c4a463a0d19489b1743a6529baaf65c275ae
Antonio Callau <antonioluiscallaucano@gmail.com>
2024-05-08 13:36:44 +0200

    solucionado error importacion de atropos, main.js

--- src/components/CardAtroposVue.vue
+++ src/components/CardAtroposVue.vue
@@ -17,12 +17,6 @@
 </template>
 
 <script>
-
-// import Atropos component
-import AtroposComponent from 'atropos/element';
-// register Atropos component
-customElements.define('atropos-component', AtroposComponent);
-
 export default {
   name: 'CardAtroposVue',
 }
--- src/main.js
+++ src/main.js
@@ -5,9 +5,14 @@ import store from './store'
 
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css"; // Import the CSS or use your own!
-
 const options = {
     // You can set your default options here
 };
+
+// import Atropos component
+import AtroposComponent from 'atropos/element';
+// register Atropos component
+customElements.define('atropos-component', AtroposComponent);
+
 
 createApp(App).use(store).use(router).use(Toast, options).mount('#app')
