f4166502cd45b97bbdca812cca20206283edb417
Antonio Callau <antonioluiscallaucano@gmail.com>
2024-05-15 20:31:01 +0200

    añadido form y submit a los inputs

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,14 +4,14 @@
     <NavVue></NavVue>
 
     <div class="bg-[#111015] text-white flex flex-col items-center pt-6 md:h-[85vh] min-h-[70vh] h-auto">
-      <div class="md:w-[30vw] h-full w-[90vw] flex flex-col md:items-center md:gap-8 gap-6 mb-4">
+      <form action="" method="POST" class="md:w-[30vw] h-full w-[90vw] flex flex-col md:items-center md:gap-8 gap-6 mb-4">
         <p class="md:text-2xl text-xl">Login</p>
         <input type="email" placeholder="Email" v-model="email" class="p-2 rounded-md w-full bg-[#111015] border border-[#14c458]">
         <input type="password" placeholder="Contraseña" v-model="password" class="p-2 rounded-md w-full bg-[#111015] border border-[#14c458]">
         <router-link to="/register" class="text-blue-500 underline w-full">¿No tienes cuenta? Regístrate</router-link>
         <router-link to="/register" class="text-blue-500 underline w-full">¿Has olvidado tu contraseña?</router-link>
-        <button class="bg-[#14c458] text-[#dadbdb] w-[120px] h-[35px] rounded-md cursor-pointer hover:border hover:border-white-600 shadow-md bg-gradient-to-br from-[#14c458] to-teal-400">Enviar</button>
-      </div>
+        <button type="submit" class="bg-[#14c458] text-[#dadbdb] w-[120px] h-[35px] rounded-md cursor-pointer hover:border hover:border-white-600 shadow-md bg-gradient-to-br from-[#14c458] to-teal-400">Enviar</button>
+      </form>
     </div>
 
     <FooterVue></FooterVue>
