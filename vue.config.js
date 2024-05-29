a63265cf88c4a53e1d367bf4655fe256d31d6814
Antonio Callau <antonioluiscallaucano@gmail.com>
2024-05-29 15:39:49 +0200

    modificado configuracion de Vue para deploy

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,8 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        // target: 'http://localhost:3000/',
+        target: 'https://proyecto-final-antonio-backend.onrender.com:3000/',
         changeOrigin: true,
       }
     }
